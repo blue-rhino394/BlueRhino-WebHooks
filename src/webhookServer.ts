de2a6d17b2738dde506e2d482bbdfbd862e563e9
Iseeicy <iseeicyhawkston@gmail.com>
2020-09-17 00:34:53 -0500

    Added auto deploy functionality

--- src/webhookServer.ts
+++ src/webhookServer.ts
@@ -1,5 +1,8 @@
 ﻿import express, { Application } from "express";
 import { createServer, Server as HTTPServer } from "http";
+import { CONNREFUSED } from "dns";
+import { randomBytes } from "crypto";
+const { exec } = require("child_process");
 const bodyParser = require("body-parser");
 const path = require('path');
 
@@ -54,11 +57,66 @@ export class webhookServer {
             
             const ref = req.body.ref;
 
-            console.log(ref);
+            // If there's no ref in the post data...
+            if (!ref) {
+
+                // Bounce!
+                res.send('..Are you github..?');
+                return;
+            }
+
+            const branch = ref.replace("refs/heads/", "");
+            var cdLocation = "";
+            var pm2Name = "";
+
+
+            switch (branch) {
+
+                case "master":
+                    cdLocation = "cd ~/production/BlueRhino-SoftwareProject/";
+                    pm2Name = "app_production";
+                    break;
+
+                case "test":
+                    cdLocation = "cd ~/test/BlueRhino-SoftwareProject/";
+                    pm2Name = "app_test";
+                    break;
+
+                default:
+                    console.log(`Recieved unknown branch '${branch}'.`);
+                    return;
+                    break;
+            }
+
+
+            this.executeShellCommand(`${cdLocation} && git pull && pm2 restart ${pm2Name}`);
             res.send('Thanks!');
         });
     }
 
+
+
+
+
+    // Execute a shell command and then print the result
+    private executeShellCommand(command: string): void {
+
+        console.log(`Executing shell command '${command}'.`);
+
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`Shell execution error: ${error.message}`);
+                return;
+            }
+
+            if (stderr) {
+                console.log(`Shell stderr: ${stderr}`);
+                return;
+            }
+
+            console.log(`Shell execution result: ${stdout}`);
+        });
+    }
 
 
 
