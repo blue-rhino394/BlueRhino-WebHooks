9ad6783a468c3296d828148d8e4d707e7a98da57
Iseeicy <iseeicyhawkston@gmail.com>
2020-09-16 23:51:53 -0500

    Created base project

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+﻿import { webhookServer } from './webhookServer';
+const commandLineArgs = require('command-line-args');
+
+
+
+
+
+//
+//  Options
+//
+
+// Define what command line arguments we want, and what type we expect
+// them to be.
+const optionDefinitions = [
+    { name: 'port', type: Number }
+];
+
+// Store 'em in a generic object, then explicitly retrieve them
+const options = commandLineArgs(optionDefinitions);
+const port = options.port || 30330;
+
+
+
+
+
+//
+//  App Execution
+//
+
+const server = new webhookServer(port);
+
+server.listen(portUsed => {
+    console.log(`Server is listening on port ${portUsed}!`);
+});
---
+++ src/webhookServer.ts
@@ -0,0 +1,75 @@
+﻿import express, { Application } from "express";
+import { createServer, Server as HTTPServer } from "http";
+const path = require('path');
+
+
+
+export class webhookServer {
+
+    // The express server that this server uses.
+    private app: Application;
+    // The HTTP Server to use with Express
+    private httpServer: HTTPServer;
+
+    // The port to run the webserver on
+    private port: number;
+
+
+
+
+
+
+
+
+
+    //
+    //  Construction an initialization
+    //
+
+    // Constructor!
+    constructor(port: number) {
+        this.initialize();
+
+        this.port = port;
+    }
+
+    // Create and configure any objects for this class
+    private initialize(): void {
+        this.app = express();
+        this.httpServer = createServer(this.app);
+
+        this.configureApp();
+    }
+
+    // Specifically configure the Express server
+    private configureApp(): void {
+        this.app.post('/github', (req, res) => {
+
+            console.log(req.body);
+            res.send('Thanks!');
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+    // Start the server and begin listening on the port provided
+    // via the constructor.
+    //
+    // Executes callback when the server has begun listening
+    public listen(callback: (port: number) => void): void {
+
+        // Start listening on the httpServer...
+        this.httpServer.listen(this.port, () => {
+
+            // Now that we're listening, execute callback
+            callback(this.port);
+        });
+    }
+}
